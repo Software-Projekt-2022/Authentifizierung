c4d49a56b9882c77be7f4136051da2c95171c936
Andreas Wegner <andreaswegner@posteo.de>
2022-06-26 19:48:25 +0200

    Added missing cors() call

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(passport.initialize());
 app.use(myMiddleware);
+app.use(cors());
 
 app.use((req, res, next) =>
 {
